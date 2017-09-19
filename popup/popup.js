3d43ba16d84129bec0eae90536dc76120b8a22ec
pea3nut <pea3nut@qq.com>
2017-09-19 10:23:28 +0800

    render UI use Vue.js 1.x-csp

--- popup/popup.js
+++ popup/popup.js
@@ -1,30 +1,44 @@
-const ActionMap ={
-    openNotepad(){
-        alert('openNotepad');
+new Vue({
+    el :'body > .panel',
+    data(){return{
+        mainList :[
+        {
+            i18n :'popup__notepad',
+            icon :'/images/skin/notepad.png',
+            action(){alert('open notepad!')},
+        },{
+            i18n :'popup__mspaint',
+            icon :'/images/skin/mspaint.png',
+            action(){alert('open mspaint!')},
+        },{
+            i18n :'popup__calc',
+            icon :'/images/skin/calc.png',
+            action(){alert('open calc!')},
+        },{
+            i18n :'popup__myComputer',
+            icon :'/images/skin/myComputer.png',
+            action(){alert('open myComputer!')},
+        },{
+            i18n :'popup__paintWebpage',
+            icon :'/images/skin/paintWebpage.png',
+            action(){alert('paint webpage!')},
+        },{
+            i18n :'popup__switchProfile',
+            icon :'/images/skin/switchProfile.png',
+            action(){alert('switch profile!')},
+        }],
+        additionalList :[
+            [{
+                i18n :'popup__manageQuickLaunch',
+                icon :'/images/skin/customizmyql.png',
+                action(){alert('manage quick launch!')},
+            }]
+        ],
+    }},
+    methods :{
+        i18n :browser.i18n.getMessage,
     },
-};
-~function(el){
-    const i18nKey ='chaz-i18n';
-    const actionKey ='chaz-action';
-    Array.from(el.getElementsByTagName('*')).forEach(function(tag){
-        if(tag.hasAttribute(i18nKey)){
-            tag.textContent =browser.i18n.getMessage(tag.getAttribute(i18nKey));
-        };
-    });
-    el.addEventListener('click',function(event){
-        var elt =event.target;
-        while(
-            !elt.hasAttribute(actionKey)
-            && elt.tagName.toLocaleLowerCase()!=='body'
-        )elt =elt.parentNode;
-
-        if(elt.hasAttribute(actionKey)){
-            var returnValue =ActionMap[elt.getAttribute(actionKey)].call(elt);
-            if(returnValue!==false)window.close();
-        };
-
-    });
-}(document.querySelector('body > .panel'));
+});
 
 //todo: can I use vue.js?
 //todo: should only has 48x48 and 96x96 icons
